8dadd73b181c4604221b01a6a6f72820328b2247
Zahra Ghorbani <zahraghorbani894@gmail.com>
2025-01-01 10:35:22 +0330

    created new note and note list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+import AddNewNote from './components/AddNewNote/AddNewNote'
+import NoteList from './components/AddNewNote/NoteList/NoteList'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notes, setNotes] = useState([]);
 
+  const handleAddNote = (newNote) => {
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="note-header">note header</div>
+      <div className="note-app">
+        <AddNewNote onAddNote={handleAddNote} />
+        <NoteList notes={notes} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/AddNewNote/AddNewNote.jsx
@@ -0,0 +1,42 @@
+import PropTypes from "prop-types";
+import { useState } from "react"
+
+function AddNewNote({ onAddNote }) {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!title || !description) return null;
+
+        const newNote = {
+            title,
+            description,
+            id: Date.now(),
+            completed: false,
+            createAt: new Date().toISOString()
+        }
+        onAddNote(newNote);
+        setTitle("");
+        setDescription("");
+
+    }
+
+    return (
+        <div className="add-new-note">
+            <h2>Add New Note</h2>
+            <form className="note-form" onSubmit={handleSubmit}>
+                <input value={title} onChange={(e) => setTitle(e.target.value)} type="text" className="text-field" placeholder="Note title" />
+                <input value={description} onChange={(e) => setDescription(e.target.value)} type="text" className="text-field" placeholder="Note Description..." />
+                <button className="btn btn--primary">Add New Note</button>
+            </form>
+        </div>
+    )
+}
+
+export default AddNewNote
+
+AddNewNote.propTypes  = {
+    onAddNote: PropTypes.func
+};
